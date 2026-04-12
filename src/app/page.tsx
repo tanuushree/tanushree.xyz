71dfa15313a4aca2f16876fb4a7721cb9af4bd2d
Tanu Shree <tanushree.wrk@gmail.com>
2026-04-12 12:22:59 +0530

    chore: update landing page components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,14 @@ import Image from "next/image";
 import About from "../components/About";
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
+import Work from "@/components/Work";
 
 export default function Home() {
   return (
     <div >
       <Hero />
+      <Work />
+      <About />
     </div>
   );
 }
